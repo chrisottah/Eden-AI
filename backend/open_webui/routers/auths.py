1fd5858b098a43d71e95d35c831d3ede224c9908
chrisottah <chrisottah@gmail.com>
2025-11-14 19:04:51 +0000

    Save local changes before updating to v0.6.36

--- backend/open_webui/routers/auths.py
+++ backend/open_webui/routers/auths.py
@@ -1087,10 +1087,7 @@ async def kingschat_login(request: Request, redirect: Optional[str] = None):
 ############################
 
 @router.post("/auth/callback")
-<<<<<<< HEAD
 @router.get("/auth/callback") 
-=======
->>>>>>> f91383b16 (added kingschat button for login)
 async def kingschat_callback(
     request: Request,
     response: Response,
